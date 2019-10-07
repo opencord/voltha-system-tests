fa9144461a56f7e96ac5a935995aec25f0f2e9ec
David Bainbridge <dbainbri@ciena.com>
2019-10-07 18:38:32 +0000

    VOL-1983 - introduce test required dependencies
    Change-Id: I96ca62d55d952af0beea8a8d7c0752761f14d986

---
+++ libraries/DependencyLibrary.py
@@ -0,0 +1,22 @@
+from robot.libraries.BuiltIn import BuiltIn
+
+class DependencyLibrary(object):
+    ROBOT_LISTENER_API_VERSION = 2
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def __init__(self):
+        self.ROBOT_LIBRARY_LISTENER = self
+        self.test_status = {}
+
+    def require_test_case(self, name):
+        key = name.lower()
+        if (key not in self.test_status):
+            BuiltIn().fail("required test case can't be found: '%s'" % name)
+
+        if (self.test_status[key] != "PASS"):
+            BuiltIn().fail("required test case failed: '%s'" % name)
+
+        return True
+
+    def _end_test(self, name, attrs):
+        self.test_status[name.lower()] = attrs["status"]
