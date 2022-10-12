66602a880eed364948cdd2ef6560aebcce7ea637
Joey Armstrong <joey@opennetworking.org>
2022-10-11 23:04:49 -0400

    Makefile makefiles/lint.mk makefiles/lint/json.mk makefiles/lint/python.mk makefiles/lint/robot.mk makefiles/lint/yaml.mk makefiles/virtualenv.mk ----    * Begin simplifying 700+ line monolithic makefile.    * Extract and repackage lint based targets beneath makefiles/lint.    * Added help target(s) to document functionality.
    Change-Id: Ieaacf59ee3023d29c4e8be7b266f4c7169111138

---
+++ makefiles/virtualenv.mk
@@ -0,0 +1,32 @@
+# -*- makefile -*-
+## -----------------------------------------------------------------------
+# Copyright 2017-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+## -----------------------------------------------------------------------
+
+# virtualenv for the robot tools
+# VOL-2724 Invoke pip via python3 to avoid pathname too long on QA jobs
+
+vst_venv-2 : vst_venv/bin/activate requirements.txt
+
+vst_venv/bin/activate:
+	virtualenv -p python3 $@ ;\
+	source ./$@/bin/activate ;\
+	python -m pip install -r requirements.txt
+
+zzz: xyz
+xyz: vst_venv/bin/activate
+	source vst_venv/bin/activate
+
+# [EOF]
