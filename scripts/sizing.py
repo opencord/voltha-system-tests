ba1e061d793a0ebc1aae9c25c6ea724abc3d9e77
Joey Armstrong <joey@opennetworking.org>
2022-11-01 16:09:40 -0400

    VOL-4805 This script is an inital attempt at capturing and documenting testing resources for interactive and programatic retrieval.  Command line arguments and eventual filesystem based detection will hints for a suite of tests to run to evaluate VOLTHA code changes.
    make check: run python unit tests
    
    usage:
      % flog.py --help
      % flog.py --usage
      % flog.py --type regression
      % flog.py --attr olt
    
    Recreate patch: start over
      o approval given by: daf & torsten
      o a cycle of squashing roaches reported by lint checking resulted
        in multiple conflicting patches in gerrit.  Abandon the mess and
        submit a clean patch.
      o Only delta introduced is using the updated standard copyright notice.
    
    Change-Id: Id43b2f2a28c038f6c4cd986c6f3907e4938722bf

--- scripts/sizing.py
+++ scripts/sizing.py
@@ -1,4 +1,6 @@
-# Copyright 2017-present Open Networking Foundation
+# -*- python -*-
+# -----------------------------------------------------------------------
+# Copyright 2022 Open Networking Foundation (ONF) and the ONF Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# -----------------------------------------------------------------------
 
 # This tool collects CPU and Memory informations for each container in the VOLTHA stack
 
