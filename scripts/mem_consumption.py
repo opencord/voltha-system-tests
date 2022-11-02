1a8f07bc099594fdeeb29b05252e1f24152a1d60
Joey Armstrong <joey@opennetworking.org>
2022-11-02 15:12:35 +0000

    Revert "VOL-4805"
    This reverts commit 44fa7d8aee85f25a1fb39c3118a7932e3d3783f0.
    
    Reason for revert: Need to rev version string for this patch.
    
    Change-Id: Ie63a8d4c56bc7c7418ee1c44dd85599c51388634

--- scripts/mem_consumption.py
+++ scripts/mem_consumption.py
@@ -1,6 +1,4 @@
-# -*- python -*-
-# -----------------------------------------------------------------------
-# Copyright 2022 Open Networking Foundation (ONF) and the ONF Contributors
+# Copyright 2021-present Open Networking Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -----------------------------------------------------------------------
 
 # This tool collects instantaneous memory usage for voltha pods
 
