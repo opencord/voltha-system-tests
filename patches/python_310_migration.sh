12c36477748cce11bcce4250ba5f861e8d15e2ec
Joey Armstrong <joey@opennetworking.org>
2022-11-26 09:40:08 -0500

    VOL-4874 - Fix local lint target use problems.
    scripts/which_deployment_owns_device.sh
    ---------------------------------------
       * Fix a few shellcheck complaints.
    
    makefiles/lint/robot.mk
    -----------------------
       * Update target logic to use && -vs- ;
       * semicolon use can mask errors.
    
    makefiles/lint/python.mk
    ------------------------
       * Update target logic to use && -vs- ;
       * Removed pylint option --py3k, switch not suppored by newer interpreters.
    
    makefiles/lint.mk
    makefiles/lint/shell.mk
    -----------------------
       * Added a lint target to invoke shellcheck on sources.
    
    Makefile
    --------
       * target:clean modified to remove generated sources.
       * target:gendocs: unique $(LIBDIRS) and replace for loop with a simple mkdir -vp call.
       * cosmetic indentation cleanup.
       * modified vst_venv target to apply patches after virtualenv created.
       * patches address failures caused by python 3.10+ requiring collections.abc
       * Ignore exit status from 310_migration for now to avoid failing other jobs.
    
    patches/lib/python3.10/site-packages/robot/utils/normalizing.py/patch
    patches/lib/python3.10/site-packages/robot/utils/robottypes3.py/patch
    ---------------------------------------------------------------------
       * Attempt to import the new collections.abc module for Mapping and
         friends so python 3.10+ can be supported.
       * Fall back to existing module import supported by python interpreters
         v3.5 <=> v3.9.
    
    patches/python_310_migration.sh
    -------------------------------
       * Self contained helper script used to generate and apply python 3.10+ patches.
    
    Followup edits
    --------------
       * Added missing license blocks reported by jenkins.
       * Updated lint checking to detect license errors so problems can be fixed prior to checkin.
       * Add set -x debugging, not clear why pyenv patching failed.
    
    Change-Id: I2b8515bb26874eb8b1564bec87353ea0c1485b61

---
+++ patches/python_310_migration.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# -----------------------------------------------------------------------
+# Copyright 2022 Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------
+
+set -euo pipefail
+
+dst="vst_venv"
+src="staging"
+pat="patches"
+
+declare -a fyls=()
+fyls+=('lib/python3.10/site-packages/robot/utils/normalizing.py')
+fyls+=('lib/python3.10/site-packages/robot/utils/robottypes3.py')
+
+echo
+echo "==========================================================================="
+echo "CMD: $0"
+echo "PWD: $(/bin/pwd)"
+echo "ARGV: $*"
+echo "==========================================================================="
+
+if [ $# -eq 0 ]; then set -- apply; fi
+
+while [ $# -gt 0 ]; do
+    opt="$1"; shift
+    case "$opt" in
+	help)
+	    cat <<EOH
+apply  - generate patches from vault source.
+backup - Archive patch directory
+gather - collect potential python files to edit.
+EOH
+	    ;;
+
+	apply)
+	    pushd "$dst" || { echo "pushd $dst failed"; exit 1; }
+	    for fyl in "${fyls[@]}";
+	    do
+		echo "$fyl"
+		patch -R -p1 < "../$pat/$fyl/patch"
+	    done
+	    popd || { echo "popd $dst failed"; exit 1; }
+	    ;;
+
+	backup)
+	    mkdir ~/backups
+	    pushd "$src" || { echo "pushd $dst failed"; exit 1; }
+	    tar czvf ~/backups/vault."$(date '+%Y%m%d%H%M%S')" "${fyls[@]}"
+	    popd || { echo "popd $dst failed"; exit 1; }
+	    ;;
+
+	gather)
+	    set -x
+	    for fyl in "${fyls[@]}";
+	    do
+		patchDir="$pat/$fyl"
+		mkdir -p "$patchDir"
+		diff -Naur "$src/$fyl" "$dst/$fyl" | tee "$pat/$fyl/patch"
+	    done
+	    find "$pat" -print
+	    set +x
+	    ;;
+	
+	*)
+	    echo "ERROR: Unknown action [$opt]"
+	    exit 1
+	    ;;
+    esac
+done
+
+# [EOF]
